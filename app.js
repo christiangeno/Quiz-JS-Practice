a9898d7a02a5c740dfa5a22419034cb5ca47f82e
Christian Geno Pasion <christiangenopasion@gmail.com>
2023-05-26 10:08:35 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+function enter() {
+    var name = document.getElementById('name').value;
+    document.getElementById('name').value = "";
+    document.querySelector('.enteryourname').style.display = "none";
+    document.querySelector('.q1').style.display = "block";
+    document.getElementById('qCount').innerHTML = "Question #1";
+  }
+
+  function next1() {
+    document.querySelector('.q1').style.display = "none";
+    document.querySelector('.q2').style.display = "block";
+    document.getElementById('qCount').innerHTML = "Question #2";
+  }
+
+  function next2() {
+    document.querySelector('.q2').style.display = "none";
+    document.querySelector('.q3').style.display = "block";
+    document.getElementById('qCount').innerHTML = "Question #3";
+  }
+
+  function next3() {
+    document.querySelector('.q3').style.display = "none";
+    document.querySelector('.q4').style.display = "block";
+    document.getElementById('qCount').innerHTML = "Question #4";
+  }
+  
+  function next4() {
+    document.querySelector('.q4').style.display = "none";
+    document.querySelector('.q5').style.display = "block";
+    document.getElementById('qCount').innerHTML = "Question #5";
+  }
+
+  function next5() {
+    document.querySelector('.q5').style.display = "none";
+    document.querySelector('.result').style.display = "block";
+    document.getElementById('qCount').innerHTML = "";
+    submitQuiz();
+  }
+
+
+  function submitQuiz() {
+    var answers = ["a", "c", "c", "a", "a"]; // Add correct answers for each question here
+    var totalQuestions = 5; // Set the total number of questions here
+    var score = 0;
+
+    for (var i = 1; i <= totalQuestions; i++) {
+      var selectedOption = document.querySelector('input[name=q' + i + ']:checked');
+      if (selectedOption && selectedOption.value === answers[i - 1]) {
+        score++;
+      }
+    }
+
+    var result = document.getElementById('result');
+    result.innerHTML = "You scored " + score + " out of " + totalQuestions + "!";
+  }
